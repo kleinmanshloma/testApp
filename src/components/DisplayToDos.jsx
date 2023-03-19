2c2f31ca102cc7194841d6179a54cfe4e29ea142
kleinmanshloma <skwebdeveloper.net@gmail.com>
2023-03-19 23:48:22 +0100

    fixed checkboxClicked

--- src/components/DisplayToDos.jsx
+++ src/components/DisplayToDos.jsx
@@ -4,14 +4,16 @@ const DisplayToDos = ({ toDoList, SetToDoList }) => {
     SetToDoList(newList);
   };
 
-  // THIS IS NOT WORKING
   const checkboxClicked = (id) => {
-    /* const newList = toDoList.filter((todo) => todo.id === id);
-    console.log(...newList);
-    const up = { ...newList, complete: true };
-    console.log(up);
-    SetToDoList({ ...newList, complete: true });
-    console.log(toDoList); */
+    toDoList.map((todo) => {
+      if (todo.id === id) {
+        todo.complete = true;
+      }
+      return todo;
+    });
+
+    SetToDoList(toDoList);
+    console.log(toDoList);
   };
 
   const handleClick = (e) => {
